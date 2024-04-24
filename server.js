ba4859a5c20a2ed1b47368a7203e9be886aa5fed
Vectorlolo <victorbri2112@gmail.com>
2024-04-23 22:25:26 -0400

    correccion required

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const axios = require('axios');
-import cors from 'cors';
+const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors());
