5c614471a3ca9bdf2cd2a7b76007ee495c414168
Vectorlolo <victorbri2112@gmail.com>
2024-04-29 19:25:48 -0400

    revision 2

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 const path = require('path');
 app.use(express.json());
 app.use(cors({ credentials: true, origin: true }));
+const port = process.env.PORT || 4000;
+
 // Cambia 'TU-CLAVE-SECRETA' por tu clave secreta
 const RECAPTCHA_V3_SECRET_KEY = process.env.RECAPTCHA_V3_SECRET_KEY;
 
@@ -44,4 +46,4 @@ app.use(express.static(__dirname));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
+app.listen(port, () => console.log(`Servidor corriendo en puerto ${port}`));
